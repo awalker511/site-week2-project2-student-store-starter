feda86aeacbea41751cd7c378547bca995f7ec39
awalker511 <awalker511@yahoo.com>
2023-06-23 14:48:41 -0700

    everything is working

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,26 @@
-// YOUR CODE HERE
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const { NotFoundError } = require("./utils/error");
+const storeRoute = require("./routes/store");
+const db = require("./data/db.json");
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cors());
+app.use("/store", storeRoute);
+app.use("");
+
+//Handle not found errors
+app.use((req, res, next) => {
+  return next(new NotFoundError());
+});
+
+//get products
+
+//GET request
+app.get("./", (req, res) => {
+  res.status(200);
+});
+module.exports = app;
